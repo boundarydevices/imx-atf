7f583472964e518edca938b38f3d02189f79798b
Troy Kisky <troy.kisky@boundarydevices.com>
2018-06-21 14:01:33 -0700

    imx8_helpers: enable a UART on crash
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- plat/imx/imx8ulp/platform.mk
+++ plat/imx/imx8ulp/platform.mk
@@ -67,6 +67,11 @@ BL32_SIZE		?=	0x2000000
 $(eval $(call add_define,BL32_BASE))
 $(eval $(call add_define,BL32_SIZE))
 
+IMX_BOOT_UART_BASE	?=	0x29870000
+$(eval $(call add_define,IMX_BOOT_UART_BASE))
+IMX_BOOT_UART_REGISTER	?=	console_lpuart_register
+$(eval $(call add_define,IMX_BOOT_UART_REGISTER))
+
 ifdef IMX8ULP_DSL_SUPPORT
 $(eval $(call add_define,IMX8ULP_DSL_SUPPORT))
 endif
