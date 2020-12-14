boundarydevices/imx-atf

commit 349a92c9b041fbc57164b893392e8d9b668afca6

make_boundary.sh: refine script
- allow to overwrite U-Boot path via UBOOT_PATH variable
- create generic function for build (buildatf())
- add parallel build based on nproc number

Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

make_boundary.sh
@@ -1,34 +1,18 @@
 #!/bin/sh
-rm -fr build
-make PLAT=imx8mm BUILD_BASE=build bl31
-cp -v build/imx8mm/release/bl31.bin ~/u-boot-imx6/bl31-iMX8MM.bin
+if [ -z "$UBOOT_PATH" ]; then UBOOT_PATH=~/u-boot-imx6; fi
 
-rm -fr build
-make PLAT=imx8mn BUILD_BASE=build bl31
-cp -v build/imx8mn/release/bl31.bin ~/u-boot-imx6/bl31-iMX8MN.bin
+buildatf() {
+	rm -fr build*
+	make PLAT=$1 BUILD_BASE=$2 bl31 -j$(nproc)
+	cp -v $2/$1/release/bl31.bin $UBOOT_PATH/$3
+}
 
-rm -fr build
-make PLAT=imx8mp BUILD_BASE=build bl31
-cp -v build/imx8mp/release/bl31.bin ~/u-boot-imx6/bl31-iMX8MP.bin
-
-rm -fr build
-make PLAT=imx8mq BUILD_BASE=build bl31
-cp -v build/imx8mq/release/bl31.bin ~/u-boot-imx6/bl31-iMX8MQ.bin
-
-rm -fr build-optee
-make PLAT=imx8mm BUILD_BASE=build-optee SPD=opteed bl31
-cp -v build-optee/imx8mm/release/bl31.bin ~/u-boot-imx6/bl31-tee-iMX8MM.bin
-
-rm -fr build-optee
-make PLAT=imx8mn BUILD_BASE=build-optee SPD=opteed bl31
-cp -v build-optee/imx8mn/release/bl31.bin ~/u-boot-imx6/bl31-tee-iMX8MN.bin
-
-rm -fr build-optee
-make PLAT=imx8mp BUILD_BASE=build-optee SPD=opteed bl31
-cp -v build-optee/imx8mp/release/bl31.bin ~/u-boot-imx6/bl31-tee-iMX8MP.bin
-
-rm -fr build-optee
-make PLAT=imx8mq BUILD_BASE=build-optee SPD=opteed bl31
-cp -v build-optee/imx8mq/release/bl31.bin ~/u-boot-imx6/bl31-tee-iMX8MQ.bin
-
-chmod a-x ~/u-boot-imx6/bl31-*.bin
+buildatf imx8mm build bl31-iMX8MM.bin
+buildatf imx8mn build bl31-iMX8MQ.bin
+buildatf imx8mp build bl31-iMX8MP.bin
+buildatf imx8mq build bl31-iMX8MQ.bin
+buildatf imx8mm build-optee bl31-tee-iMX8MM.bin
+buildatf imx8mn build-optee bl31-tee-iMX8MQ.bin
+buildatf imx8mp build-optee bl31-tee-iMX8MP.bin
+buildatf imx8mq build-optee bl31-tee-iMX8MQ.bin
+chmod a-x $UBOOT_PATH/bl31-*.bin

commit 515fb041a3876b698b9f8e72bdfeb135d8a98623

make_boundary.sh: fix script to include SPD
Fixes: 349a92c9b ("make_boundary.sh: refine script")

Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

make_boundary.sh
@@ -3,16 +3,16 @@ if [ -z "$UBOOT_PATH" ]; then UBOOT_PATH=~/u-boot-imx6; fi
 
 buildatf() {
 	rm -fr build*
-	make PLAT=$1 BUILD_BASE=$2 bl31 -j$(nproc)
-	cp -v $2/$1/release/bl31.bin $UBOOT_PATH/$3
+	make PLAT=$1 BUILD_BASE=$2 SPD=$3 bl31 -j$(nproc)
+	cp -v $2/$1/release/bl31.bin $UBOOT_PATH/$4
 }
 
-buildatf imx8mm build bl31-iMX8MM.bin
-buildatf imx8mn build bl31-iMX8MQ.bin
-buildatf imx8mp build bl31-iMX8MP.bin
-buildatf imx8mq build bl31-iMX8MQ.bin
-buildatf imx8mm build-optee bl31-tee-iMX8MM.bin
-buildatf imx8mn build-optee bl31-tee-iMX8MQ.bin
-buildatf imx8mp build-optee bl31-tee-iMX8MP.bin
-buildatf imx8mq build-optee bl31-tee-iMX8MQ.bin
+buildatf imx8mm build none bl31-iMX8MM.bin
+buildatf imx8mn build none bl31-iMX8MQ.bin
+buildatf imx8mp build none bl31-iMX8MP.bin
+buildatf imx8mq build none bl31-iMX8MQ.bin
+buildatf imx8mm build-optee opteed bl31-tee-iMX8MM.bin
+buildatf imx8mn build-optee opteed bl31-tee-iMX8MQ.bin
+buildatf imx8mp build-optee opteed bl31-tee-iMX8MP.bin
+buildatf imx8mq build-optee opteed bl31-tee-iMX8MQ.bin
 chmod a-x $UBOOT_PATH/bl31-*.bin