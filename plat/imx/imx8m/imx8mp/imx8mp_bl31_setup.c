93865aa9b531c5b9a41131b426d3f077ce88339b
Gary Bisson <gary.bisson@boundarydevices.com>
2020-12-29 15:04:00 +0100

    imx8m*_bl31_setup: remove RDC limitation on UART ports
    So that all UART ports can be accessed by both A & M cores by default.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
+++ plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -62,7 +62,9 @@ static const struct imx_rdc_cfg rdc[] = {
 
 
 	/* peripherals domain permission */
+#if 0
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
+#endif
 	RDC_PDAPn(RDC_PDAP_WDOG1, D0R | D0W),
 	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
 
